d7a9c425df33503bedb60b89fc0c399f69c8ecdd
Nacho35 <ignacioevil@gmail.com>
2023-01-20 17:46:10 -0300

    agregando animaciones

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import me from "../assets/yo.jpg";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
     <section className="bg-mate3 font-lato w-full min-h-screen py-4 lg:min-h-screen lg:pt-4 md:min-h-screen md:pt-4">
       <div className="container max-h-fit w-full mx-auto flex px-5 md:flex-col flex-col items-center lg:flex-row md:flex md:text-center">
         <div className="lg:flex-grow md:w-full lg:pr-24 md:pt-14 flex flex-col md:items-center lg:items-start md:text-justify mb-16 py-4 lg:my-24 md:mb-auto items-center text-center">
-          <h1 className="sm:text-4xl text-3xl mb-4 font-semibold text-white">
+          <motion.h1
+            initial={{ opacity: 0, scale: 1, x: 1000 }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              boxShadow: "10px 10px 0 rgba(0, 0, 0, 0.2)",
+            }}
+            transition={{ duration: 2 }}
+            className="sm:text-4xl text-3xl mb-4 font-semibold text-white">
             Acerca de mí
-          </h1>
-          <p className="mb-8 leading-relaxed text-gold font-semibold">
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, scale: 1, x: 1000 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 2 }}
+            className="mb-8 leading-relaxed text-gold font-semibold">
             Hola, mi nombre es Ignacio y soy un desarrollador apasionado por la
             tecnología. Mi objetivo a largo plazo es seguir creciendo
             profesionalmente en el desarrollo web, y en un futuro ingresar al
@@ -28,22 +41,34 @@ const About = () => {
             esfuerzo constantemente por mejorar mis habilidades y conocimientos
             técnicos. Me considero una persona curiosa y siempre dispuesta a
             aprender cosas nuevas.
-          </p>
-          <div className="flex justify-center">
+          </motion.p>
+          <motion.div
+            initial={{ opacity: 0, x: 1000 }}
+            animate={{ opacity: 1, x: 0 }}
+            whileHover={{
+              scale: 1.1,
+              boxShadow: "0px 0px 8px rgb(255,204,109)",
+              textShadow: "0px 0px 2px rgb(61, 62, 66)",
+            }}
+            className="flex justify-center">
             <Link
               to="/feature"
               className="flex mx-auto text-gold bg-transparent border-2 border-gold rounded border-solid py-2 px-6 focus:outline-none text-lg font-semibold hover:text-mate hover:bg-gold">
               Saber Mas!
             </Link>
-          </div>
+          </motion.div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-3/4/2 md:flex md:justify-center md:pt-20 w-5/6">
+        <motion.div
+          initial={{ x: 1000 }}
+          animate={{ x: 0 }}
+          transition={{ delay: 0.5, type: "spring", duration: 4 }}
+          className="lg:max-w-lg lg:w-full md:w-3/4/2 md:flex md:justify-center md:pt-20 w-5/6">
           <img
             className="object-cover object-center rounded-lg"
             alt="hero"
             src={me}
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
 import bg from "../assets/bg.mp4";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <section className="font-lato relative w-full min-h-full">
       <video src={bg} autoPlay loop muted></video>
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center absolute justify-center bottom-0 top-0 left-0 right-0 lg:absolute lg:bottom-0 lg:top-0 lg:left-0 lg:right-0">
-        <div className="lg:flex-grow lg:items-center md:w-full flex flex-col md:items-center md:text-center items-center text-center">
+        <motion.div
+          initial={{ opacity: 0, scale: 1 }}
+          animate={{ opacity: 1, x: 0, y: 0 }}
+          transition={{ delay: 0.5, duration: 3 }}
+          className="lg:flex-grow lg:items-center md:w-full flex flex-col md:items-center md:text-center items-center text-center">
           <div className="text-base font-semibold text-white lg:text-3xl text-center md:text-2xl">
             Hola Soy!{" "}
             <h1 className="flex flex-col text-3xl text-gold font-semibold my-2  lg:text-7xl lg:text-center md:text-6xl">
@@ -21,11 +26,11 @@ const Home = () => {
               <Link
                 to="/form"
                 className="inline-flex border-2 border-gold rounded border-solid py-2 px-6 mb-2 focus:outline-none text-base lg:text-xl md:text-lg font-semibold text-mate bg-gold">
-                Contactarme!
+                Contactame!
               </Link>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
